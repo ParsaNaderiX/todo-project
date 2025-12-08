cb2c65b9d5479c0981755457b3c4ff145b7dd809
ParsaNaderiX <parsanaderi2004@gmail.com>
2025-12-09 01:01:48 +0330

    Remove deprecated in-memory storage and ports layer

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,8 +1,6 @@
-from .todo_service import TodoService
 from .project_service import ProjectService
 from .task_service import TaskService
 __all__ = [
-	"TodoService",
 	"ProjectService",
 	"TaskService",
 ]
